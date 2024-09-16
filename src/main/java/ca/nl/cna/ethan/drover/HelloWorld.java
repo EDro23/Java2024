7409eaa95064fca292655434d3ff9a78f2aa5e8d
edro23 <ethandrover@hotmail.com>
2024-09-16 09:28:15 -0230

    Hello World

--- src/main/java/ca/nl/cna/ethan/drover/HelloWorld.java
+++ src/main/java/ca/nl/cna/ethan/drover/HelloWorld.java
@@ -1,5 +1,10 @@
 package ca.nl.cna.ethan.drover;
 
+/**
+ * Our first Application! A simple Hello World
+ *
+ * @author ethan drover
+ */
 public class HelloWorld {
     public static void main(String[] args)
     {
