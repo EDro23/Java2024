7a4c2afa0a65714fe7d9e8d084f7d2110fa0118c
edro23 <ethandrover@hotmail.com>
2024-10-23 12:23:52 -0230

    Oct23...

--- src/main/java/ca/nl/cna/ethan/drover/Assignment4/FunWithFibonacci.java
+++ src/main/java/ca/nl/cna/ethan/drover/Assignment4/FunWithFibonacci.java
@@ -1,8 +1,47 @@
 package ca.nl.cna.ethan.drover.Assignment4;
 
+import java.util.Scanner;
+
+/**
+ * Class for having fun with Fibonacci calculator
+ * @author Ethan.Drover
+ */
 public class FunWithFibonacci {
 
     public static void main(String[] args) {
-        System.out.printf("Fun with Fibonacci!\n");
+        System.out.println("Fun with Fibonacci!\n");
+
+        Scanner input = new Scanner(System.in);
+
+        String choice = "";
+
+        do {
+            printMenu();
+            choice = input.next();
+
+            if (choice.equals("A")) {
+                //TODO Method for printing fibonacci numbers up to a certain number
+                System.out.println("Echo A");
+            } else if (choice.equals("B")) {
+                //TODO Method for printing the first X amount of fibonacci numbers from 0 to a specific number
+                System.out.println("Echo B");
+            } else if (choice.equals("C")) {
+                //TODO Method for checking if the inputted number is a fibonacci number
+                System.out.println("Echo C");
+            } else {
+                if (!choice.equals("Q")) {
+                    System.out.println("Not a valid choice, Try again!");
+                    System.out.printf("Choice: " + choice);
+                }
+            }
+        } while (!choice.equals("Q"));
+    }
+
+    private static void printMenu() {
+        System.out.println("Menu");
+        System.out.println("A");
+        System.out.println("B");
+        System.out.println("C");
+        System.out.println("Q to quit");
     }
 }
