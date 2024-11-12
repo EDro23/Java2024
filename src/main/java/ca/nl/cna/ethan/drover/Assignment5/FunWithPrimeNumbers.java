0530cc9f197459e68430fbb24098089f396a0879
Ethan <Ethandrover@hotmail.com>
2024-11-11 21:54:13 -0330

    A5 Complete...

--- src/main/java/ca/nl/cna/ethan/drover/Assignment5/FunWithPrimeNumbers.java
+++ src/main/java/ca/nl/cna/ethan/drover/Assignment5/FunWithPrimeNumbers.java
@@ -1,7 +1,73 @@
 package ca.nl.cna.ethan.drover.Assignment5;
 
+import java.util.Scanner;
+
+/**
+ * Class to test the prime numbers calculator with a built-in menu:
+ * 1. Display all prime numbers up to a value
+ * 2. Display all primes between two numbers
+ * 3. Display the unique prime factorization of a number
+ * 4. Display the unique prime factorizations up to a limit
+ * 5. Exit
+ *
+ */
 public class FunWithPrimeNumbers {
+
     public static void main(String[] args) {
-        System.out.println("Fun With Prime Numbers");
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nFun With Prime Numbers");
+            System.out.println("1. Display all prime numbers up to a value");
+            System.out.println("2. Display all primes between two numbers");
+            System.out.println("3. Display the unique prime factorization of a number");
+            System.out.println("4. Display the unique prime factorizations up to a limit");
+            System.out.println("5. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.print("Enter a value: ");
+                int limit = scanner.nextInt();
+                System.out.println("Prime numbers up to " + limit + ":");
+                for (int i = 2; i <= limit; i++) {
+                    if (PrimeNumberCalculator.isPrime(i)) {
+                        System.out.print(i + " ");
+                    }
+                }
+                System.out.println();
+            } else if (choice == 2) {
+                System.out.print("Enter the start value: ");
+                int start = scanner.nextInt();
+                System.out.print("Enter the end value: ");
+                int end = scanner.nextInt();
+                System.out.println("Prime numbers between " + start + " and " + end + ":");
+                for (int i = start; i <= end; i++) {
+                    if (PrimeNumberCalculator.isPrime(i)) {
+                        System.out.print(i + " ");
+                    }
+                }
+                System.out.println();
+            } else if (choice == 3) {
+                System.out.print("Enter a number: ");
+                int number = scanner.nextInt();
+                String factorization = PrimeNumberCalculator.getUniquePrimeFactorization(number);
+                System.out.println("Unique prime factorization of " + number + ": " + factorization);
+            } else if (choice == 4) {
+                System.out.print("Enter a limit: ");
+                int factorizationLimit = scanner.nextInt();
+                System.out.println("Prime factorizations up to " + factorizationLimit + ":");
+                for (int i = 2; i <= factorizationLimit; i++) {
+                    String factors = PrimeNumberCalculator.getUniquePrimeFactorization(i);
+                    System.out.println(i + " = " + factors);
+                }
+            } else if (choice == 5) {
+                running = false;
+            } else {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        }
+        scanner.close();
     }
 }
--- src/main/java/ca/nl/cna/ethan/drover/Assignment5/PrimeNumberCalculator.java
+++ src/main/java/ca/nl/cna/ethan/drover/Assignment5/PrimeNumberCalculator.java
@@ -1,16 +1,56 @@
 package ca.nl.cna.ethan.drover.Assignment5;
 
+/**
+ * Prime number calculator class
+ * Includes 2 methods, one to determine if a number is prime and another to get the unique prime factorization
+ */
 public class PrimeNumberCalculator {
 
-    public int isPrime(int number) {
-        // TODO Has a method called isPrime that determines whether an integer is prime.
-        return number;
+    /**
+     * Determines whether an integer is prime
+     *
+     * @param number the number that is being checked
+     * @return true if number is prime, false if not prime
+     */
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public int getUniquePrimeFactorization ( int number) {
-        // TODO Has method called getUniquePrimeFactorization that returns the unique prime factorization of an integer as a String.
-        return number;
-        // TODO both methods should be static
-    }
+    /**
+     * Determines the unique prime factorization of a given integer and returns it as a formatted string.
+     * The method takes an integer input, finds its prime factors, and returns a string representation
+     * of the factors separated by " x ".
+     *
+     * @param number the integer to be factorized
+     * @return a string representing the unique prime factorization of the input number
+     *         or the number itself if it is less than or equal to 1
+     */
+    public static String getUniquePrimeFactorization(int number) {
+        if (number <= 1) {
+            return String.valueOf(number);
+        }
 
+        String result = "";
+        for (int i = 2; i <= number; i++) {
+            if (isPrime(i) && number % i == 0) {
+                while (number % i == 0) {
+                    if (result.isEmpty()) {
+                        result += i;
+                    } else {
+                        result += " x " + i;
+                    }
+                    number = number / i;
+                }
+            }
+        }
+        return result;
+    }
 }
